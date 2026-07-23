63aeb004032845a794de4ea5280dde7d1364150f
jessytpt <jessytpt.dev@gmail.com>
2026-07-23 14:11:40 -0500

    Deploy backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import cancha
 
 app = FastAPI(title="API Canchas")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:4200",              # Angular en local
+        # "https://tu-front.onrender.com",    # cuando tu compa despliegue
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(cancha.router)
+
 
 @app.get("/")
 def root():
-    return {"mensaje": "hola mundo"}
+    return {"mensaje": "API Canchas activa"}
